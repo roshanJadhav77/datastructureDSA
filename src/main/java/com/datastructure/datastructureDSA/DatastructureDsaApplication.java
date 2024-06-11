151e48d0b9d82b5ec7f430cb6a8af529b5ed3d72
Suvarna <user@users-MacBook-Air.local>
2024-06-11 23:28:16 +0530

    first commit

---
+++ src/main/java/com/datastructure/datastructureDSA/DatastructureDsaApplication.java
@@ -0,0 +1,13 @@
+package com.datastructure.datastructureDSA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatastructureDsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatastructureDsaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/datastructure/datastructureDSA/DatastructureDsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datastructure.datastructureDSA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatastructureDsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
