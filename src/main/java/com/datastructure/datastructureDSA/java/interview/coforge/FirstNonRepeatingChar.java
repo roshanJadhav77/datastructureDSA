be9b24029a6a7b18877d9f181449b392a6c1bba9
Roshan Jadhav <roshanjadhav33@gmail.com>
2024-09-05 21:57:38 +0530

    adding new line

--- src/main/java/com/datastructure/datastructureDSA/java/interview/coforge/FirstNonRepeatingChar.java
+++ src/main/java/com/datastructure/datastructureDSA/java/interview/coforge/FirstNonRepeatingChar.java
@@ -3,6 +3,9 @@ package com.datastructure.datastructureDSA.java.interview.coforge;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+/**
+ * This class finds the first non-repeating character in a string.
+ */
 public class FirstNonRepeatingChar {
     public static void main(String[] args) {
 
@@ -11,6 +14,14 @@ public class FirstNonRepeatingChar {
 
     }
 
+    /**
+     * This method takes a string as input and returns the first non-repeating
+     * character.
+     *
+     * @param str The string to be searched.
+     * @return The first non-repeating character in the string, or null if there
+     *         is none.
+     */
     static Character getFirstNonRepeatingChar(String str){
         Map<Character, Integer> map = new LinkedHashMap<>();
 
