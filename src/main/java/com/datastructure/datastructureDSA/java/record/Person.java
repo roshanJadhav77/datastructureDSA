8cf91ae957e1b79d64ca97067487fc475ebc98e2
Roshan Jadhav <roshanjadhav33@gmail.com>
2024-11-21 23:05:42 +0530

    added SOLID principle

---
+++ src/main/java/com/datastructure/datastructureDSA/java/record/Person.java
@@ -0,0 +1,8 @@
+package com.datastructure.datastructureDSA.java.record;
+
+public record Person(String name,
+                     int age,
+                     String lastName,
+                     String wifeName,
+                     String relationship) {}
+
